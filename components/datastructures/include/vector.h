b2099c3edc715943798650a8d9346bd4e37fee79
Jonas Kuß <jonaskuss@protonmail.com>
2023-11-06 11:12:24 +0100

    WIP

--- components/datastructures/include/vector.h
+++ components/datastructures/include/vector.h
@@ -1,1 +1,254 @@
 #pragma once
+
+#include <cassert>
+#include <cstring>
+#include <utility>
+
+template<typename Vector>
+class VectorIterator {
+public:
+    using ValueType = Vector::ValueType;
+
+    VectorIterator(ValueType* ptr)
+        : m_ptr(ptr)
+    {
+    }
+
+    VectorIterator& operator++()
+    {
+        m_ptr++;
+        return *this;
+    }
+
+    VectorIterator operator++(int)
+    {
+        VectorIterator it = *this;
+        ++(*this);
+        return it;
+    }
+
+    VectorIterator& operator--()
+    {
+        m_ptr--;
+        return *this;
+    }
+
+    VectorIterator operator--(int)
+    {
+        VectorIterator it = *this;
+        --(*this);
+        return it;
+    }
+
+    ValueType& operator[](unsigned index)
+    {
+        return *(m_ptr + index);
+    }
+
+    ValueType* operator->()
+    {
+        return m_ptr;
+    }
+
+    ValueType& operator*()
+    {
+        return *m_ptr;
+    }
+
+    bool operator==(VectorIterator const& other)
+    {
+        return m_ptr == other.m_ptr;
+    }
+
+    bool operator!=(VectorIterator const& other)
+    {
+        return !(*this == other);
+    }
+
+private:
+    ValueType* m_ptr;
+};
+
+template<typename T>
+class Vector {
+public:
+    using ValueType = T;
+    using Iterator = VectorIterator<Vector<T>>
+
+    Vector()
+    {
+        m_capacity = 0;
+        m_size = 0;
+        m_data = malloc(0);
+    }
+
+    Vector(unsigned capacity)
+    {
+        capacity = capacity;
+        m_size = 0;
+        m_data = malloc(capacity * sizeof(T));
+    }
+
+    Vector(Vector<T> const& other)
+    {
+        set_capacity(other.m_capacity);
+        m_size = other.m_size;
+        memcpy(m_data, other.m_data, m_size * sizeof(T));
+    }
+
+    ~Vector()
+    {
+        clear();
+        ::operator delete(m_data, m_capacity * sizeof(T));
+    }
+
+    unsigned size() const
+    {
+        return m_size;
+    }
+
+    unsigned capacity()
+    {
+        return m_size;
+    }
+
+    bool is_empty()
+    {
+        return m_size == 0;
+    }
+
+    bool is_full()
+    {
+        return m_size == m_capacity;
+    }
+
+    // TODO: Write an iterator as well!
+    T& operator[](unsigned index)
+    {
+        assert(index < m_size);
+        return m_data[index];
+    }
+
+    T const& operator[](unsigned index) const
+    {
+        assert(index < m_size);
+        return m_data[index];
+    }
+
+    void insert(T const& item, unsigned index)
+    {
+        assert(index < m_size);
+        m_data[index] = item;
+    }
+
+    void insert(T&& item, unsigned index)
+    {
+        assert(index < m_size);
+        ::operator new(m_data[index]) T(std::move(item));
+    }
+
+    // NOTE: also implement something like EmplaceBack in std::vector?
+    void append(T const& item)
+    {
+        if (needs_to_grow()) {
+            set_capacity(next_capacity());
+        }
+
+        m_data[m_size++] = item;
+    }
+
+    void append(T&& item)
+    {
+        if (needs_to_grow()) {
+            set_capacity(next_capacity());
+        }
+
+        ::operator new(m_data[m_size++]) T(std::move(item));
+    }
+
+    void swap(unsigned i1, unsigned i2)
+    {
+        assert(i1 < size);
+        assert(i2 < size);
+
+        T tmp = data[i1];
+        data[i1] = data[i2];
+        data[i2] = tmp;
+    }
+
+    T remove(unsigned index)
+    {
+        assert(index < size);
+
+        T removed = data[index];
+        data[index] = data[--v->size];
+
+        if (needs_to_shrink()) {
+            set_capacity(m_size);
+        }
+
+        return removed;
+    }
+
+    void clear()
+    {
+        for (unsigned i = 0; i < m_size; i++) {
+            m_data[i].~T();
+        }
+
+        m_size = 0;
+    }
+
+    Iterator begin()
+    {
+        return Iterator(m_data);
+    }
+
+    Iterator end()
+    {
+        return Iterator(m_data + m_size);
+    }
+
+private:
+    constexpr unsigned resize_factor = 1.25;
+
+    unsigned m_capacity = 0;
+    unsigned m_size = 0;
+    T* m_data = nullptr;
+
+    bool needs_to_shrink()
+    {
+        // NOTE: I don't know if this is the right approach.
+        // We want to aggresively shrink large allocations,
+        // but this would disproportionally affect small vectors,
+        // because less removes are needed to fall below the resize factor.
+        // Maybe we should add a lower bound where it doesn't matter,
+        // if we shrink.
+        return m_size > 0 && m_capacity / m_size > resize_factor;
+    }
+
+    bool needs_to_grow()
+    {
+        return m_size == m_capacity;
+    }
+
+    unsigned next_capacity()
+    {
+        return m_capacity < 2 ? 2 : m_capacity * resize_factor;
+    }
+
+    void set_capacity(unsigned capacity)
+    {
+        assert(capacity >= size);
+
+        T* data = (T*)::operator new(capacity * sizeof(T));
+        for (unsigned i = 0; i < m_size; i++) {
+            ::operator new(&data[i]) T(std::move(m_data[i]));
+            m_data[i].~T();
+        }
+        ::operator delete(m_data, m_capacity * sizeof(T));
+
+        m_data = data;
+        m_capacity = capacity;
+    }
+};
--- components/datastructures/test/test_datastructures.cpp
+++ components/datastructures/test/test_datastructures.cpp
@@ -1,2 +1,193 @@
 #include "unity.h"
 #include "vector.h"
+
+TEST_CASE("Creating an empty vector", "[vector]")
+{
+    Vector<int> v;
+    TEST_ASSERT_EQUAL(v.size(), 0)
+}
+
+TEST_CASE("Creating a vector with a specific capacity", "[vector]")
+{
+    Vector<int> v = Vector(10);
+
+    TEST_ASSERT_EQUAL(v.size(), 0);
+    TEST_ASSERT_EQUAL(v.capacity(), 10);
+}
+
+TEST_CASE("Get an item from a vector", "[vector]")
+{
+    Vector<int> v;
+    v.append(42);
+
+    TEST_ASSERT_EQUAL(v[0] == 42);
+}
+
+TEST_CASE("Get an item from a vector out of bounds", "[vector][fails]")
+{
+    Vector<int> v;
+    unsigned invalid = v[3];
+}
+
+TEST_CASE("Insert an item at a specific index", "[vector]")
+{
+    Vector<int> v = Vector(5);
+
+    int items[5] = { 3, 87, 2, 25, 48 };
+    for (unsigned i = 0; i < 5; i++) {
+        v.append(items[i]);
+    }
+
+    v.insert(60, 3);
+    v.insert(10, 1);
+
+    int items_after[5] = { 3, 10, 2, 60, 48 };
+    for (unsigned i = 0; i < 5; i++) {
+        TEST_ASSERT_EQUAL(v[i], items_after[i]);
+    }
+}
+
+TEST_CASE("Append multiple items", "[vector]")
+{
+    int items[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    Vector<int> v;
+
+    for (unsigned i = 0; i < 10; i++) {
+        v.append(items[i]);
+        TEST_ASSERT_EQUAL(v[i], items[i]);
+    }
+}
+
+TEST_CASE("Swap two items", "[vector]")
+{
+    Vector<int> v = Vector(5);
+
+    int items[5] = { 3, 87, 2, 25, 48 };
+    for (unsigned i = 0; i < 5; i++) {
+        v.append(items[i]);
+    }
+
+    v.swap(v, 1, 4);
+    v.swap(v, 2, 3);
+
+    int items_after[5] = { 3, 48, 25, 2, 87 };
+    for (unsigned i = 0; i < 5; i++) {
+        TEST_ASSERT_EQUAL(v[i], items_after[i]);
+    }
+}
+
+Test(vector, empty)
+{
+    int items[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int_vector_t* v = int_vector_create();
+    cr_assert(int_vector_empty(v));
+
+    for (unsigned i = 0; i < 10; i++) {
+        int_vector_append(v, items[i]);
+        cr_assert(!int_vector_empty(v));
+    }
+
+    int_vector_clear(v);
+    cr_assert(int_vector_empty(v));
+
+    int_vector_destroy(v);
+}
+
+Test(vector, full)
+{
+    int items[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int_vector_t* v = int_vector_create_with_capacity(10);
+
+    for (unsigned i = 0; i < 10; i++) {
+        int_vector_append(v, items[i]);
+    }
+
+    cr_assert(int_vector_full(v));
+    int_vector_destroy(v);
+}
+
+Test(vector, remove)
+{
+    int items[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int_vector_t* v = int_vector_create();
+
+    for (unsigned i = 0; i < 10; i++) {
+        int_vector_append(v, items[i]);
+    }
+
+    cr_assert(int_vector_remove(v, 7) == items[7]);
+    cr_assert(int_vector_remove(v, 3) == items[3]);
+    cr_assert(int_vector_remove(v, 0) == items[0]);
+    cr_assert(int_vector_size(v) == 7);
+
+    int items_after[7] = { 9, 1, 2, 8, 4, 5, 6 };
+
+    for (unsigned i = 0; i < 7; i++) {
+        cr_assert(int_vector_at(v, i) == items_after[i]);
+    }
+
+    int_vector_destroy(v);
+}
+
+Test(vector, remove_invalid, .signal = SIGABRT)
+{
+    int_vector_t* v = int_vector_create();
+    int_vector_remove(v, 13);
+    int_vector_destroy(v);
+}
+
+Test(vector, clear)
+{
+    int_vector_t* v = int_vector_create();
+    int_vector_append(v, 33);
+    int_vector_clear(v);
+    cr_assert(int_vector_size(v) == 0);
+}
+
+Test(vector, clone)
+{
+    int items[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int_vector_t* v = int_vector_create();
+
+    for (unsigned i = 0; i < 10; i++) {
+        int_vector_append(v, items[i]);
+    }
+
+    int_vector_t* cloned = int_vector_clone(v);
+
+    cr_assert(v->capacity == cloned->capacity);
+    cr_assert(v->size == cloned->size);
+
+    for (unsigned i = 0; i < int_vector_size(v); i++) {
+        cr_assert(int_vector_at(v, i) == int_vector_at(cloned, i));
+    }
+}
+
+Test(vector, destroy)
+{
+    int_vector_t* v = int_vector_create();
+    int_vector_destroy(v);
+}
+
+Test(vector, destroy_invalid, .signal = SIGABRT)
+{
+    int_vector_t* v = int_vector_create();
+    v->size = 9001;
+    int_vector_destroy(v);
+}
+
+typedef struct test_struct {
+    int a;
+    void* b;
+    char d;
+} test_struct_vector;
+
+MAKE_SPECIFIC_VECTOR_HEADER(test_struct_vector, test_struct_vector)
+MAKE_SPECIFIC_VECTOR_SOURCE(test_struct_vector, test_struct_vector)
+
+Test(vector, generic_create)
+{
+    test_struct_vector_vector_t* v = test_struct_vector_vector_create();
+    cr_assert(test_struct_vector_vector_size(v) == 0);
+    test_struct_vector_vector_destroy(v);
+}
