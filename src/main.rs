82df149460106a74185d7103fa5693a95891e275
Jonas Kuß <jonaskuss@protonmail.com>
2023-11-16 16:18:46 +0100

    Main: Send COPE broadcast packets instead of simple string

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(test_runner::run)]
 
+mod packet;
 mod test_runner;
 
 use esp_idf_svc::espnow::{EspNow, PeerInfo, BROADCAST};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
 use std::{thread::sleep, time::Duration};
+
+use crate::packet::{Packet, PacketID};
 
 fn main() {
     esp_idf_svc::sys::link_patches(); //Needed for esp32-rs
@@ -32,15 +35,17 @@ fn main() {
         espnow_driver.add_peer(peer_info).unwrap();
     }
 
-    let mut broadcast_counter: u32 = 0;
+    let mut packet_id: PacketID = 0;
     loop {
-        broadcast_counter += 1;
+        let packet = Packet::new(packet_id, 'A', 'B');
+        let serialized = bincode::serialize(&packet).unwrap();
         espnow_driver
-            .send(BROADCAST, "Is anybody there?".as_bytes())
+            .send(BROADCAST, &serialized.as_slice())
             .unwrap();
 
-        println!("Sent broadcast message no. {}!", broadcast_counter);
+        println!("Sent {:?} as broadcast!", packet);
         sleep(Duration::new(2, 0));
+        packet_id += 1;
     }
 }
 
