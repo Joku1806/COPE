601b660329e50fe791297943d5b33efabdc25d29
Magnus Kroner <magnus.kroner@gmail.com>
2024-01-23 12:47:20 +0100

    addded ack.rs

---
+++ cope/src/packet/ack.rs
@@ -0,0 +1,21 @@
+use cope_config::types::node_id::NodeID;
+use serde::{Deserialize, Serialize};
+use super::CodingInfo;
+
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Ack {
+    source: NodeID,
+    packets: Vec<CodingInfo>,
+}
+
+impl Ack {
+    pub fn source(&self) -> NodeID {
+        self.source
+    }
+
+    pub fn packets(&self) -> &[CodingInfo] {
+        self.packets.as_ref()
+    }
+}
