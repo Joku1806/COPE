7cd84117f0db5a5de2e4aeb073ba2c9366134317
Jonas Kuß <jonaskuss@protonmail.com>
2024-01-26 19:14:40 +0100

    Meta: Add script for filtering statistics on the ESP
    The script will run the application and then write only lines with the
    format STATS $filename $data to a csv. All other lines will just be
    printed to stdout.

---
+++ esp/Meta/collect_statistics.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+esp_port=$1
+
+process_output() {
+    while read -r line; do
+        if [[ $line == STATS* ]]; then
+            mapfile -d " " -t parts < <(printf "%s" "$line")
+            # NOTE: For the format of statistics output,
+            # see the implementation of EspStatsLogger in esp_channel.rs
+            filename="${parts[1]}"
+            data="${parts[2]}"
+            echo "${data}" >> "${filename}"
+        else
+            echo "${line}"
+        fi
+    done
+}
+
+# Between different computers, the specific serial port
+# will also most likely be different. Because of this,
+# we support passing the serial port as a parameter.
+if [ -n "$esp_port" ]; then
+    cargo run -- -p "$esp_port" | process_output
+else
+    # If you did not set a default serial port using cargo,
+    # espflash will ask about it and block script execution!
+    cargo run | process_output
+fi
